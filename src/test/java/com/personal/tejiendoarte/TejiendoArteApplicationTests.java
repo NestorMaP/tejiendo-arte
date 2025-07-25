fdcc2b3adc515de389d23aa0625ec58e5eb24764
Nestor <mateoprats.nestor@gmail.com>
2025-07-25 16:52:19 +0200

    Create project

---
+++ src/test/java/com/personal/tejiendoarte/TejiendoArteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.tejiendoarte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TejiendoArteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
