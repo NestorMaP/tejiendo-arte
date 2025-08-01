8217ac352fe966d7489f6886a0291d6cf0cd6f5a
Nestor <mateoprats.nestor@gmail.com>
2025-08-01 13:47:02 +0200

    Feat: WebSecurityConfiguration Beans

--- src/main/java/com/personal/tejiendoarte/security/WebSecurityConfiguration.java
+++ src/main/java/com/personal/tejiendoarte/security/WebSecurityConfiguration.java
@@ -3,9 +3,13 @@ package com.personal.tejiendoarte.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -31,6 +35,16 @@ public class WebSecurityConfiguration {
                 )
                 .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() { return new BCryptPasswordEncoder(); }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
+    throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
 }
