419000d40fe382f5ec0cf210fc5cc8138645004c
Nestor <mateoprats.nestor@gmail.com>
2025-08-01 13:44:52 +0200

    Feat: WebSecurityConfiguration

---
+++ src/main/java/com/personal/tejiendoarte/security/WebSecurityConfiguration.java
@@ -0,0 +1,36 @@
+package com.personal.tejiendoarte.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.apache.catalina.webresources.TomcatURLStreamHandlerFactory.disable;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfiguration {
+
+    private final JwtRequestFilter jwtRequestFilter;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> disable())
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/authenticate","/sign-up").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                )
+                .addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+}
