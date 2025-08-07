eb84f0106879b2331d5a0498129f247b13377770
Nestor <mateoprats.nestor@gmail.com>
2025-08-07 12:09:06 +0200

    Feat: Create new CategoryRepository

---
+++ src/main/java/com/personal/tejiendoarte/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.personal.tejiendoarte.repository;
+
+
+import com.personal.tejiendoarte.entity.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category,Long> {
+}
