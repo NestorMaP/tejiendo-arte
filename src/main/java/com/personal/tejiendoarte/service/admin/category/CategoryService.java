b4b6bd8e37f9f6884dce59b93c2139dac72c1469
Nestor <mateoprats.nestor@gmail.com>
2025-08-07 12:15:01 +0200

    Feat[CategoryService]:Add createCategory

--- src/main/java/com/personal/tejiendoarte/service/admin/category/CategoryService.java
+++ src/main/java/com/personal/tejiendoarte/service/admin/category/CategoryService.java
@@ -1,4 +1,10 @@
 package com.personal.tejiendoarte.service.admin.category;
 
+import com.personal.tejiendoarte.dto.CategoryDto;
+import com.personal.tejiendoarte.entity.Category;
+
 public interface CategoryService {
+
+    Category createCategory(CategoryDto categoryDto);
+
 }
