6c7aa612ea22c3698521b263d6ecd6d907f50af3
Nestor <mateoprats.nestor@gmail.com>
2025-08-07 12:19:04 +0200

    Feat[CategoryController]: Logic for create category

---
+++ src/main/java/com/personal/tejiendoarte/controller/admin/AdminCategoryController.java
@@ -0,0 +1,27 @@
+package com.personal.tejiendoarte.controller.admin;
+
+import com.personal.tejiendoarte.dto.CategoryDto;
+import com.personal.tejiendoarte.entity.Category;
+import com.personal.tejiendoarte.service.admin.category.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminCategoryController {
+
+    private final CategoryService categoryService;
+
+    @PostMapping("category")
+    public ResponseEntity<Category> createCategory(@RequestBody CategoryDto categoryDto) {
+        Category category = categoryService.createCategory(categoryDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(category);
+    }
+
+}
