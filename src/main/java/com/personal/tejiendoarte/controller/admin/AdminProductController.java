820db735f01c6c63479872b988761fcd9907e648
Nestor <mateoprats.nestor@gmail.com>
2025-08-08 12:15:01 +0200

    Feat: AddProduct to ProductController

--- src/main/java/com/personal/tejiendoarte/controller/admin/AdminProductController.java
+++ src/main/java/com/personal/tejiendoarte/controller/admin/AdminProductController.java
@@ -1,9 +1,15 @@
 package com.personal.tejiendoarte.controller.admin;
 
+import com.personal.tejiendoarte.dto.ProductDto;
 import com.personal.tejiendoarte.service.admin.adminproduct.AdminProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/admin")
@@ -11,5 +17,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminProductController {
 
     private final AdminProductService adminProductService;
+
+    public ResponseEntity<ProductDto> addProduct(@ModelAttribute ProductDto requestProductDto) throws IOException {
+        ProductDto responseProductDto = adminProductService.addProduct(requestProductDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseProductDto);
+    }
 
 }
