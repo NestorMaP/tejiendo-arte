94c3ebcd51741f0e7ec879b92aaee63ecd8b908a
Nestor <mateoprats.nestor@gmail.com>
2025-08-10 17:27:49 +0200

    Feat: New method to look for Product by name in repository

--- src/main/java/com/personal/tejiendoarte/repository/ProductRepository.java
+++ src/main/java/com/personal/tejiendoarte/repository/ProductRepository.java
@@ -3,5 +3,9 @@ package com.personal.tejiendoarte.repository;
 import com.personal.tejiendoarte.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product,Long> {
+
+    List<Product> findAllByNameContaining(String title);
 }
