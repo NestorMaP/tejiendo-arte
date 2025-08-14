cb4d95683da890d5cec02d330db43c5b4a198078
Nestor <mateoprats.nestor@gmail.com>
2025-08-14 10:49:35 +0200

    Feat: Order Entity

---
+++ src/main/java/com/personal/tejiendoarte/entity/CartItems.java
@@ -0,0 +1,4 @@
+package com.personal.tejiendoarte.entity;
+
+public class CartItems {
+}
---
+++ src/main/java/com/personal/tejiendoarte/entity/Order.java
@@ -0,0 +1,44 @@
+package com.personal.tejiendoarte.entity;
+
+import com.personal.tejiendoarte.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Data
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String description;
+
+    private Date date;
+
+    private Long amount;
+
+    private String address;
+
+    private String payment;
+
+    private OrderStatus status;
+
+    private Long totalAmount;
+
+    private Long discount;
+
+    private UUID trackingId;
+
+    @OneToOne(cascade = CascadeType.MERGE)
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "order")
+    private List<CartItems> cartItems;
+
+}
