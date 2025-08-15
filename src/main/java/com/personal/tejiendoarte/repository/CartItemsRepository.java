0834b10801bd4444da8611cae98a150634f0f06a
Nestor <mateoprats.nestor@gmail.com>
2025-08-15 12:05:42 +0200

    Feat: Repository for Items in Cart

---
+++ src/main/java/com/personal/tejiendoarte/repository/CartItemsRepository.java
@@ -0,0 +1,9 @@
+package com.personal.tejiendoarte.repository;
+
+import com.personal.tejiendoarte.entity.CartItems;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartItemsRepository extends JpaRepository<CartItems, Long> {
+}
