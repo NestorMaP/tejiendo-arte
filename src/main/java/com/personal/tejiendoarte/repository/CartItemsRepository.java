6ca6b99642790f99d4f5e85118f175c004b9f741
Nestor <mateoprats.nestor@gmail.com>
2025-08-15 12:13:46 +0200

    Feat: Needed methods in different repositories to complete addProductToCart method in CartServiceImpl

--- src/main/java/com/personal/tejiendoarte/repository/CartItemsRepository.java
+++ src/main/java/com/personal/tejiendoarte/repository/CartItemsRepository.java
@@ -4,6 +4,10 @@ import com.personal.tejiendoarte.entity.CartItems;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CartItemsRepository extends JpaRepository<CartItems, Long> {
+
+    Optional<CartItems> findByProductIdAndOrderIdAndUserId(Long productId, Long orderId, Long userId);
 }
--- src/main/java/com/personal/tejiendoarte/repository/OrderRepository.java
+++ src/main/java/com/personal/tejiendoarte/repository/OrderRepository.java
@@ -1,9 +1,12 @@
 package com.personal.tejiendoarte.repository;
 
 import com.personal.tejiendoarte.entity.Order;
+import com.personal.tejiendoarte.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    Order findByUserIdAndStatus(Long userId, OrderStatus status);
 }
