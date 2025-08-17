bb43ed02a1d75c756deb80f78a4449ab4b1c004a
Nestor <mateoprats.nestor@gmail.com>
2025-08-17 21:18:47 +0200

    Feat: Coupon repository

---
+++ src/main/java/com/personal/tejiendoarte/repository/CouponRepository.java
@@ -0,0 +1,9 @@
+package com.personal.tejiendoarte.repository;
+
+import com.personal.tejiendoarte.entity.Coupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CouponRepository extends JpaRepository<Coupon, Long> {
+}
