ccda5798d730077fd8ad779a508db7a4b2794152
Nestor <mateoprats.nestor@gmail.com>
2025-08-17 21:27:13 +0200

    Feat: Coupon service with new method

--- src/main/java/com/personal/tejiendoarte/service/admin/coupon/AdminCouponService.java
+++ src/main/java/com/personal/tejiendoarte/service/admin/coupon/AdminCouponService.java
@@ -1,4 +1,11 @@
 package com.personal.tejiendoarte.service.admin.coupon;
 
+import com.personal.tejiendoarte.dto.CouponDto;
+import com.personal.tejiendoarte.entity.Coupon;
+import com.personal.tejiendoarte.exceptions.ValidationException;
+
 public interface AdminCouponService {
+
+    public CouponDto createCoupon(Coupon coupon) throws ValidationException;
+
 }
--- src/main/java/com/personal/tejiendoarte/service/admin/coupon/AdminCouponServiceImpl.java
+++ src/main/java/com/personal/tejiendoarte/service/admin/coupon/AdminCouponServiceImpl.java
@@ -2,8 +2,9 @@ package com.personal.tejiendoarte.service.admin.coupon;
 
 import com.personal.tejiendoarte.dto.CouponDto;
 import com.personal.tejiendoarte.entity.Coupon;
+import com.personal.tejiendoarte.exceptions.ValidationException;
 import com.personal.tejiendoarte.repository.CouponRepository;
-import jakarta.validation.ValidationException;
+import com.personal.tejiendoarte.utils.mapper.CouponMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +14,13 @@ public class AdminCouponServiceImpl implements AdminCouponService {
 
     private final CouponRepository couponRepository;
 
+    private final CouponMapper couponMapper;
+
     public CouponDto createCoupon(Coupon coupon) throws ValidationException {
         if(couponRepository.existsByCode(coupon.getCode())) {
             throw new ValidationException("Coupon already exists");
         }
+        return couponMapper.mapToDto(couponRepository.save(coupon));
     }
 
 }
