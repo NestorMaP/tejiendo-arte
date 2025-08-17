4fb3055727811e2fb3020f606fb576a78f9ff337
Nestor <mateoprats.nestor@gmail.com>
2025-08-17 11:05:44 +0200

    Feate CartItemsDto

---
+++ src/main/java/com/personal/tejiendoarte/dto/CartItemsDto.java
@@ -0,0 +1,17 @@
+package com.personal.tejiendoarte.dto;
+
+public class CartItemsDto {
+
+    private Long id;
+
+    private Long productId;
+
+    private String productName;
+
+    private Long unitPrice;
+
+    private Long quantity;
+
+    private double lineTotalPrice;
+
+}
