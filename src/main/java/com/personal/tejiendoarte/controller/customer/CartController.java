73b8a3141af1d25d5b2aa9906aa170748df18b9e
Nestor <mateoprats.nestor@gmail.com>
2025-08-17 12:10:18 +0200

    Cart Controller with new endpoint

--- src/main/java/com/personal/tejiendoarte/controller/customer/CartController.java
+++ src/main/java/com/personal/tejiendoarte/controller/customer/CartController.java
@@ -5,10 +5,7 @@ import com.personal.tejiendoarte.service.customer.cart.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/customer")
@@ -16,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class CartController {
 
     private final CartService cartService;
+
+    @GetMapping("/cart/{userId}")
+    public ResponseEntity<?> getCartByUserId(@PathVariable Long userId) {
+        return ResponseEntity.status(HttpStatus.OK).body(cartService.getCartByUserId(userId));
+    }
 
     @PostMapping("/cart")
     public ResponseEntity<?> addProductToCart(@RequestBody AddProductInCartDto addProductInCartDto) {
