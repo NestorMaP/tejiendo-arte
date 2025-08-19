8eaba5cef061fde3e3110a0354f2cb0f20158668
Nestor <mateoprats.nestor@gmail.com>
2025-08-19 17:02:05 +0200

    Feat[AdminOrderService]: New method to get all orders

--- src/main/java/com/personal/tejiendoarte/service/admin/adminOrder/AdminOrderService.java
+++ src/main/java/com/personal/tejiendoarte/service/admin/adminOrder/AdminOrderService.java
@@ -1,4 +1,11 @@
 package com.personal.tejiendoarte.service.admin.adminOrder;
 
+import com.personal.tejiendoarte.dto.OrderDto;
+
+import java.util.List;
+
 public interface AdminOrderService {
+
+    public List<OrderDto> getAllOrders();
+
 }
--- src/main/java/com/personal/tejiendoarte/service/admin/adminOrder/AdminOrderServiceImpl.java
+++ src/main/java/com/personal/tejiendoarte/service/admin/adminOrder/AdminOrderServiceImpl.java
@@ -1,11 +1,15 @@
 package com.personal.tejiendoarte.service.admin.adminOrder;
 
 import com.personal.tejiendoarte.dto.OrderDto;
+import com.personal.tejiendoarte.entity.Order;
+import com.personal.tejiendoarte.enums.OrderStatus;
 import com.personal.tejiendoarte.repository.OrderRepository;
+import com.personal.tejiendoarte.utils.mapper.OrderMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +17,16 @@ public class AdminOrderServiceImpl implements AdminOrderService {
 
     private final OrderRepository orderRepository;
 
+    private final OrderMapper orderMapper;
+
     public List<OrderDto> getAllOrders() {
 
+        List<Order> orderList = orderRepository.findAllByStatusIn(List.of(
+                OrderStatus.PLACED,
+                OrderStatus.SHIPPED,
+                OrderStatus.DELIVERED));
+
+        return orderList.stream().map(orderMapper::mapToDto).collect(Collectors.toList());
     }
 
 }
