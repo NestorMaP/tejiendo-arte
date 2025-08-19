59c11c28042e59251ea342c77a414d37271ea3e8
Nestor <mateoprats.nestor@gmail.com>
2025-08-19 12:14:59 +0200

    Feat: New Dto

---
+++ src/main/java/com/personal/tejiendoarte/dto/PlaceOrderDto.java
@@ -0,0 +1,14 @@
+package com.personal.tejiendoarte.dto;
+
+import lombok.Data;
+
+@Data
+public class PlaceOrderDto {
+
+    private Long userId;
+
+    private String address;
+
+    private String orderDescription;
+
+}
