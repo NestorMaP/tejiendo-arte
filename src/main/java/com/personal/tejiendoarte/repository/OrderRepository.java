2131eb5e698ee6a6e1fe42e1022ecb0d048d1bea
Nestor <mateoprats.nestor@gmail.com>
2025-08-19 16:58:32 +0200

    Feat[OrderRepository]: New Query to filter by status

--- src/main/java/com/personal/tejiendoarte/repository/OrderRepository.java
+++ src/main/java/com/personal/tejiendoarte/repository/OrderRepository.java
@@ -5,8 +5,12 @@ import com.personal.tejiendoarte.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
     Order findByUserIdAndStatus(Long userId, OrderStatus status);
+
+    List<Order> findAllByStatusIn(List<OrderStatus> orderStatusList)
 }
