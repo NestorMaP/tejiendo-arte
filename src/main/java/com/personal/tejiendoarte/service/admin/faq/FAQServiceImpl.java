c7c8017224ce94d50eecbe3e41d7efe8f6973331
Nestor <mateoprats.nestor@gmail.com>
2025-08-21 00:37:38 +0200

    Feat: FAQMapper

--- src/main/java/com/personal/tejiendoarte/service/admin/faq/FAQServiceImpl.java
+++ src/main/java/com/personal/tejiendoarte/service/admin/faq/FAQServiceImpl.java
@@ -1,9 +1,31 @@
 package com.personal.tejiendoarte.service.admin.faq;
 
+import com.personal.tejiendoarte.dto.FAQDto;
+import com.personal.tejiendoarte.entity.FAQ;
+import com.personal.tejiendoarte.entity.Product;
+import com.personal.tejiendoarte.repository.FAQRepository;
+import com.personal.tejiendoarte.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class FAQServiceImpl implements FAQService {
+
+    private final FAQRepository faqRepository;
+
+    private final ProductRepository productRepository;
+
+    public FAQDto postFAQ(Long productId, FAQDto faqDto) {
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+
+        if(optionalProduct.isEmpty()) return null;
+
+        FAQ faq = new FAQ();
+
+
+    }
+
 }
---
+++ src/main/java/com/personal/tejiendoarte/utils/mapper/FAQMapper.java
@@ -0,0 +1,21 @@
+package com.personal.tejiendoarte.utils.mapper;
+
+import com.personal.tejiendoarte.dto.FAQDto;
+import com.personal.tejiendoarte.entity.FAQ;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class FAQMapper {
+
+    private FAQDto mapToDto(FAQ faq) {
+        return FAQDto.builder()
+                .id(faq.getId())
+                .question(faq.getQuestion())
+                .answer(faq.getAnswer())
+                .productId(faq.getProduct().getId())
+                .build();
+    }
+
+}
