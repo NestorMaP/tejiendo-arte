add78112c7fd64f56d625b89886a05af4e1af358
Nestor <mateoprats.nestor@gmail.com>
2025-08-20 17:36:27 +0200

    Feat: New endpoint for AdminOrderController

--- src/main/java/com/personal/tejiendoarte/controller/admin/AdminOrderController.java
+++ src/main/java/com/personal/tejiendoarte/controller/admin/AdminOrderController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,14 @@ public class AdminOrderController {
     @GetMapping("/placedOrders")
     public ResponseEntity<List<OrderDto>> getAllPlacedOrders() {
         return ResponseEntity.status(HttpStatus.OK).body(adminOrderService.getAllOrders());
+    }
+
+    //TODO: Improve the following endpoint
+    @GetMapping("/order/{orderId}/{status}")
+    public ResponseEntity<?> changeOrderStatus(@PathVariable Long orderId, @PathVariable String status) {
+        OrderDto orderDto = adminOrderService.changeOrderStatus(orderId, status);
+        if(orderDto == null) return new ResponseEntity<>("Something went wrong", HttpStatus.BAD_REQUEST);
+        return ResponseEntity.status(HttpStatus.OK).body(orderDto);
     }
 
 }
