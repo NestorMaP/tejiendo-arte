a6791e8fedcd6fa0a298a0dc5d3270e59281c724
Nestor <mateoprats.nestor@gmail.com>
2025-08-22 00:28:36 +0200

    Feat: New method in service interface

--- src/main/java/com/personal/tejiendoarte/service/customer/review/ReviewService.java
+++ src/main/java/com/personal/tejiendoarte/service/customer/review/ReviewService.java
@@ -1,9 +1,14 @@
 package com.personal.tejiendoarte.service.customer.review;
 
 import com.personal.tejiendoarte.dto.OrderedProductsResponseDto;
+import com.personal.tejiendoarte.dto.ReviewDto;
+
+import java.io.IOException;
 
 public interface ReviewService {
 
     public OrderedProductsResponseDto getOrderedProductsDetailsByOrderId(Long orderId);
+
+    public ReviewDto giveReview(ReviewDto reviewDto) throws IOException;
 
 }
