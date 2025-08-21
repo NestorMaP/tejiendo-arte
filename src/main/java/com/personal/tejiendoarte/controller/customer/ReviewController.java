dfec68c115beba7e60e5130a73b6b3cb159a2cfe
Nestor <mateoprats.nestor@gmail.com>
2025-08-21 11:19:31 +0200

    Feat: New Controller for customer

---
+++ src/main/java/com/personal/tejiendoarte/controller/customer/ReviewController.java
@@ -0,0 +1,25 @@
+package com.personal.tejiendoarte.controller.customer;
+
+import com.personal.tejiendoarte.dto.OrderedProductsResponseDto;
+import com.personal.tejiendoarte.service.customer.review.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/customer")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @GetMapping("/ordered-products/{orderId}")
+    public ResponseEntity<OrderedProductsResponseDto> getOrderedProductsDetailsByOrderId(@PathVariable Long orderId) {
+        return ResponseEntity.status(HttpStatus.OK).body(reviewService.getOrderedProductsDetailsByOrderId(orderId));
+    }
+
+}
