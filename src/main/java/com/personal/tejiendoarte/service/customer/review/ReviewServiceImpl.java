3deabd92a0b3c1230b31c32d5f3e48f2527a04ef
Nestor <mateoprats.nestor@gmail.com>
2025-08-22 00:14:09 +0200

    Feat: New method to save a new review in ReviewServiceImpl

--- src/main/java/com/personal/tejiendoarte/service/customer/review/ReviewServiceImpl.java
+++ src/main/java/com/personal/tejiendoarte/service/customer/review/ReviewServiceImpl.java
@@ -2,8 +2,13 @@ package com.personal.tejiendoarte.service.customer.review;
 
 import com.personal.tejiendoarte.dto.OrderedProductsResponseDto;
 import com.personal.tejiendoarte.dto.ProductDto;
+import com.personal.tejiendoarte.dto.ReviewDto;
 import com.personal.tejiendoarte.entity.Order;
+import com.personal.tejiendoarte.entity.Product;
+import com.personal.tejiendoarte.entity.User;
 import com.personal.tejiendoarte.repository.OrderRepository;
+import com.personal.tejiendoarte.repository.ProductRepository;
+import com.personal.tejiendoarte.repository.UserRepository;
 import com.personal.tejiendoarte.utils.mapper.ProductMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,7 +22,12 @@ import java.util.List;
 public class ReviewServiceImpl implements ReviewService {
 
     private final OrderRepository orderRepository;
+    private final ProductRepository productRepository;
+    private final UserRepository userRepository;
+    private final ReviewRepository reviewRepository;
+
     private final ProductMapper productMapper;
+    private final ReviewMapper reviewMapper;
 
     public OrderedProductsResponseDto getOrderedProductsDetailsByOrderId(Long orderId) {
         Order order = orderRepository.findById(orderId)
@@ -35,5 +45,18 @@ public class ReviewServiceImpl implements ReviewService {
                 .productDtoList(productDtoList)
                 .orderAmount(order.getAmount())
                 .build();
+    }
+
+    public ReviewDto giveReview(ReviewDto reviewDto) {
+        Product product = productRepository.findById(reviewDto.getProductId())
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "Product " + reviewDto.getProductId() + " not Found")
+                );
+        User user = userRepository.findById(reviewDto.getUserId())
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.NOT_FOUND, "User " + reviewDto.getUserId() + " not Found")
+                );
+
+        return reviewRepository.save(reviewMapper.mapToEntity(reviewDto));
     }
 }
