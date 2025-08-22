102b6b9a7f8036ea5c4515e84bd0101aff26573a
Nestor <mateoprats.nestor@gmail.com>
2025-08-22 21:58:34 +0200

    Feat: New controller

---
+++ src/main/java/com/personal/tejiendoarte/controller/customer/TrackingController.java
@@ -0,0 +1,27 @@
+package com.personal.tejiendoarte.controller.customer;
+
+import com.personal.tejiendoarte.dto.OrderDto;
+import com.personal.tejiendoarte.service.customer.cart.CartService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/customer")
+public class TrackingController {
+
+    private final CartService cartService;
+
+    @GetMapping("/order/{trackingId}")
+    public ResponseEntity<OrderDto> searchOrderByTrackingId(@PathVariable UUID trackingId) throws IOException {
+        return ResponseEntity.status(HttpStatus.OK).body(cartService.searchOrderByTrackingId(trackingId));
+    }
+}
